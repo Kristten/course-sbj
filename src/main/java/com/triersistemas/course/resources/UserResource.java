4ce2d86c758d8d77dcb080d8489a081fe808b35a
Kristten <kristten258@gmail.com>
2022-06-23 23:29:15 -0300

    Third commit

--- src/main/java/com/triersistemas/course/resources/UserResource.java
+++ src/main/java/com/triersistemas/course/resources/UserResource.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.triersistemas.course.entities.User;
+import com.triersistemas.course.entities.Users;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+	public ResponseEntity<Users> findAll(){
+		Users u = new Users(1L, "Maria", "maria@gmail.com", "99999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 
