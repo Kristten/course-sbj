0c87b65a8684874276167328623522d1bf172f54
Kristten <kristten258@gmail.com>
2022-06-23 22:40:23 -0300

    Projected Created

---
+++ src/test/java/com/triersistemas/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triersistemas.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
