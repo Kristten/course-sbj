adcbc1620bd06a3190febf5f20ec5a7eba206317
Kristten <kristten258@gmail.com>
2022-06-23 23:40:47 -0300

    Fourth commit

---
+++ src/main/java/com/triersistemas/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.triersistemas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.triersistemas.course.entities.Users;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+
+}
