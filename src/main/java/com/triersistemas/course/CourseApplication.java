370af3452420e5cc222a09106e894584562297e3
Kristten <kristten258@gmail.com>
2022-07-04 23:22:12 -0300

    Final

--- src/main/java/com/triersistemas/course/CourseApplication.java
+++ src/main/java/com/triersistemas/course/CourseApplication.java
@@ -2,7 +2,8 @@ package com.triersistemas.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+@EntityScan(basePackages = "com.triersistemas.course.entities")
 @SpringBootApplication
 public class CourseApplication {
 
